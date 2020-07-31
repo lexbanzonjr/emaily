7dd6101a836e4a0eda3961c4409b5ba3572c0fee
Lex Banzon Jr <lex.banzon@evisions.com>
2020-07-31 16:18:54 -0700

    Typo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passpord-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 const app = express();
 

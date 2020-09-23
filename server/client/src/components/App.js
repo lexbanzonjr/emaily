095216aa0c15f0cbacf41e31733f486d65b8f1d9
Lex Banzon Jr <lexbanzonjr@gmail.com>
2020-09-23 12:41:20 -0700

    Created App component

---
+++ server/client/src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = (x) => {
+  return <div>Hi There!</div>;
+};
+
+export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render();
+import App from './components/App';
 
+ReactDOM.render(<App />, document.querySelector('#root'));

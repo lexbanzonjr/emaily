7f20fd6ad16e8af891139a509a32f2cffbbd5c07
Lex Banzon Jr <lexbanzonjr@gmail.com>
2020-09-25 16:31:02 -0700

    Add react router components

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -7,7 +7,18 @@ const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = (x) => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;

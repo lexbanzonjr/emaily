3d55886f17afc2d910c2329d830df02228e2d8f8
Lex Banzon Jr <lexbanzonjr@gmail.com>
2020-12-28 15:20:05 -0800

    Hook onCancel to onClick

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -4,7 +4,7 @@ const SurvryFormReview = ({onCancel}) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button className="yellow darken-3 btn-flat">
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
     </div>

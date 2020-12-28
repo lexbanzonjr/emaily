9e723390619d761605ca957ec8114f8279fae438
Lex Banzon Jr <lexbanzonjr@gmail.com>
2020-12-28 14:09:48 -0800

    Created SurveyFormReview

---
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SurvryFormReview = () => {
+  return (
+    <div>
+      <h5>Please confirm your entries</h5>
+    </div>
+  );
+};
+
+export default SurvryFormReview;
+

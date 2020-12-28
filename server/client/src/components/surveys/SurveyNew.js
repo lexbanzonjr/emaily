24946a90c9ea77460410654d2db9a45318931f7d
Lex Banzon Jr <lexbanzonjr@gmail.com>
2020-12-28 14:17:55 -0800

    Add state showFormReview

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import SurveyForm from './SurveyForm';
 
 class SurveyNew extends Component {
+  state = {showFormReview: false};
+
   render() {
     return (
       <div>

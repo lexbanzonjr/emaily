a4b12c9e622d1a1275a7499e2f3c7d924a7ad505
Lex Banzon Jr <lexbanzonjr@gmail.com>
2020-12-28 16:01:47 -0800

    Add 'Send Survey' button

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -20,6 +20,9 @@ const SurvryFormReview = ({onCancel, formValues}) => {
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
+      <button className="green btn-flat right">
+        Send Survey<i className="material-icons right">email</i>
+      </button>
     </div>
   );
 };

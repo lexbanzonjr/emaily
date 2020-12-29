e97d5d2e6ea23915a5db397ecdb8778d22d56af9
Lex Banzon Jr <lexbanzonjr@gmail.com>
2020-12-28 16:10:05 -0800

    Import actions to set onClick submitSurvey

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import React from 'react';
 import {connect} from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurvryFormReview = ({onCancel, formValues}) => {
+const SurvryFormReview = ({onCancel, formValues, submitSurvey}) => {
   const reviewFields = _.map(formFields, ({name, label}) => {
     return (
       <div key={name}>
@@ -23,7 +24,10 @@ const SurvryFormReview = ({onCancel, formValues}) => {
       >
         Back
       </button>
-      <button className="green btn-flat right white-text">
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues)}
+      >
         Send Survey<i className="material-icons right">email</i>
       </button>
     </div>
@@ -36,4 +40,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(SurvryFormReview);
+export default connect(mapStateToProps, actions)(SurvryFormReview);
